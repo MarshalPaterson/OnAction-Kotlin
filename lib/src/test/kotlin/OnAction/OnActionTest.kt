ea300235e1ae0994196e7c33f30744bf32936d65
Paterson <marshal.paterson@star.com.au>
2021-12-24 11:45:41 +1100

    Added new library.

---
+++ lib/src/test/kotlin/OnAction/OnActionTest.kt
@@ -0,0 +1,18 @@
+package OnAction
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class OnActionTest {
+    @Test fun onAction() {
+        val ONE_ACTION = "ONE_ACTION"
+        val MSG_TXT = "NEW TEXT"
+
+        OnAction.addOnAction(ONE_ACTION, object : OnAction.OnActionListener {
+            override fun onAction(it: Any) {
+                assertTrue(it == "")
+            }
+        })
+        OnAction.doAction(ONE_ACTION, MSG_TXT)
+    }
+}
